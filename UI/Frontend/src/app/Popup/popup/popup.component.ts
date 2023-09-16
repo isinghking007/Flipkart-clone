b1f88fd8d8d758dc5eefdbc070f3e1a220de7507
isinghking007 <007ravinash@gmail.com>
2023-09-17 02:37:07 +0530

    Login Popup Implementation

---
+++ UI/Frontend/src/app/Popup/popup/popup.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit} from '@angular/core';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-popup',
+  templateUrl: './popup.component.html',
+  styleUrls: ['./popup.component.css']
+})
+export class PopupComponent implements OnInit {
+
+
+  constructor() {}
+  ngOnInit(): void {
+    
+  }
+
+}
